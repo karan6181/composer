f39c650e8bfcf62df15c3e13372f1fecca43dd29
Karan Jariwala <karankjariwala@gmail.com>
2022-11-22 23:41:52 -0800

    disabled cudnn for deterministic

--- composer/utils/reproducibility.py
+++ composer/utils/reproducibility.py
@@ -113,6 +113,7 @@ def configure_deterministic_mode():
     torch.use_deterministic_algorithms(True)
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.enabled = False
     # See https://pytorch.org/docs/stable/generated/torch.use_deterministic_algorithms.html
     # and https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
     os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
