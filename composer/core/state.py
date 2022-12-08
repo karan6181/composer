900a3ab1a265bce304357b7206923a5a555fd0d3
Karan Jariwala <karankjariwala@gmail.com>
2022-12-07 21:59:34 -0800

    add false

--- composer/core/state.py
+++ composer/core/state.py
@@ -561,7 +561,7 @@ class State(Serializable):
 
         dataloader = self._dataloader_of(self.train_dataloader)
         if hasattr(dataloader, 'state_dict'):
-            obj['train'] = dataloader.state_dict(int(self.timestamp.sample_in_epoch.value))
+            obj['train'] = dataloader.state_dict(int(self.timestamp.sample_in_epoch.value), False)
 
         for evaluator in self.evaluators:
             dataloader = self._dataloader_of(evaluator)
